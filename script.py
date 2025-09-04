3a329dcb702869597625c7a0b6af0f1d1d4794c3
laurita-reyes <laura.reyes204677@gmail.com>
2025-09-04 13:56:25 -0700

    fixed issue with cronjob

--- script.py
+++ script.py
@@ -8,13 +8,11 @@ from process import fix_locations
 def main():
     # returns the GitHub jobs added daily
     file_name = git_api.git_api()
-    file = open(file_name, 'r', newline='')
+    file = open(file_name, 'r')
     df = pd.read_csv(file)
     process.fix_locations(df)
     process.fix_arrow(df)
     df.dropna(axis=0, how='any', subset=['company', 'role', 'location', 'link', 'age']).to_csv(file_name, index=False)
-
-
 
 if __name__ == '__main__':
     main()
